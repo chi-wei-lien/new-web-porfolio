94a835a00296b63c8df6097faf927ba62a9784e3
chi-wei-lien <lien2@purdue.edu>
2021-12-24 19:40:34 -0500

    deploy backend to heroku

--- api/src/app.ts
+++ api/src/app.ts
@@ -24,14 +24,14 @@ const server = new Server();
 server.app.use('/api', server.router);
 
 // Have Node serve the files for our built React app
-server.app.use(express.static(path.resolve(__dirname, '/client')));
+// server.app.use(express.static(path.resolve(__dirname, '/client')));
 
 // All other GET requests not handled before will return our React app
-server.app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// server.app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
 
 // make server listen on some port
-((port = process.env.APP_PORT || 5000) => {
+((port = process.env.PORT) => {
     server.app.listen(port, () => console.log(`> Listening on port ${port}`));
 })();
