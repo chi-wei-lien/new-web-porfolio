63856a47abfd0ac95bb33e1e7d02dd5661c50ce2
chi-wei-lien <lien2@purdue.edu>
2021-12-28 20:10:35 -0500

    change wilson to willy

--- client/src/components/index/LifeMotto.tsx
+++ client/src/components/index/LifeMotto.tsx
@@ -12,13 +12,13 @@ class LifeMotto extends Component {
             <Code propsColor="#a3a3a3" propsCode="//life motto"></Code>
           </div>
           <div className="inline">
-            <Code propsColor="#80ddff" propsCode="wilson&nbsp;"></Code>
+            <Code propsColor="#80ddff" propsCode="willy&nbsp;"></Code>
             <Code propsColor="#e3e3e3" propsCode="=&nbsp;"></Code>
             <Code propsColor="#a3a3a3" propsCode="Dream()"></Code>
           </div>
           <div className="inline">
             <Code propsColor="#ff96de" propsCode="while&nbsp;"></Code>
-            <Code propsColor="#e3e3e3" propsCode="wilson."></Code>
+            <Code propsColor="#e3e3e3" propsCode="willy."></Code>
             <Code propsColor="#ff8787" propsCode="success&nbsp;"></Code>
             <Code propsColor="#e3e3e3" propsCode="==&nbsp;"></Code>
             <Code propsColor="#e3e3e3" propsCode="false"></Code>
@@ -27,14 +27,14 @@ class LifeMotto extends Component {
             <Code propsColor="#ff96de" propsCode="&nbsp;&nbsp;&nbsp;&nbsp;try:"></Code>
           </div>
           <div className="inline">
-            <Code propsColor="#e3e3e3" propsCode="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;wilson."></Code>
+            <Code propsColor="#e3e3e3" propsCode="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;willy."></Code>
             <Code propsColor="#87a7ff" propsCode="work_hard()"></Code>
           </div>
           <div className="inline">
             <Code propsColor="#ff96de" propsCode="&nbsp;&nbsp;&nbsp;&nbsp;except:"></Code>
           </div>
           <div className="inline">
-            <Code propsColor="#ff96de" propsCode="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;wilson."></Code>
+            <Code propsColor="#ff96de" propsCode="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;willy."></Code>
             <Code propsColor="#87a7ff" propsCode="start_again()"></Code>
           </div>
           <div className="inline">
@@ -42,7 +42,7 @@ class LifeMotto extends Component {
             <Code propsColor="#e3e3e3" propsCode="("></Code>
             <Code propsColor="#80ddff" propsCode="type"></Code>
             <Code propsColor="#e3e3e3" propsCode="("></Code>
-            <Code propsColor="#e3e3e3" propsCode="wilson."></Code>
+            <Code propsColor="#e3e3e3" propsCode="willy."></Code>
             <Code propsColor="#ff8787" propsCode="success"></Code>
             <Code propsColor="#e3e3e3" propsCode="))"></Code>
           </div>
--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -9,13 +9,13 @@ class MyNavbar extends Component {
     return (
       <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className="my-navbar" fixed="top">
       <Container>
-      <Navbar.Brand href="#home">Chi-Wei Lien</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">Willy, Chi-Wei Lien</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
         </Nav>
         <Nav>
-          {/* <Nav.Link as={Link} to="/show_home">show_home()</Nav.Link> */}
+          <Nav.Link as={Link} to="/">show_home()</Nav.Link>
           <Nav.Link as={Link} to="/who_am_i">who_am_i()</Nav.Link>
           <Nav.Link href="#show_blogs">show_blogs()</Nav.Link>
           <Nav.Link href="#show_art_gallery">show_art_gallery()</Nav.Link>
