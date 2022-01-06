9679b045b7ca1881158bcf90c6b21de1c2249da3
chi-wei-lien <lien2@purdue.edu>
2022-01-06 17:23:22 -0500

    add blog edit page

--- client/src/components/blogs/blogedit.tsx
+++ client/src/components/blogs/blogedit.tsx
@@ -4,13 +4,26 @@ import axios from "axios";
 import Error from '../error/error';
 
 import { Container } from 'react-bootstrap';
+import { Editor } from '@tinymce/tinymce-react';
 
-import '../../style/index/index.css';
+import '../../style/blogs/blogedit.css';
 
 //import boostrap css
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 const Blogedit = () => {
+  const [content, setContent] = useState<string>('');
+
+  const handleSubmit = (event: any) => {
+    console.log(content);
+    event.preventDefault();
+  }
+
+  const handleChange = (content: string) => {
+    setContent(content);
+  }
+
   /**
    * If the user is not one of the admin, it will kick hime
    * to the no access page
@@ -24,9 +37,28 @@ const Blogedit = () => {
   return (
     <>
       <MyNavbar />
-      <Container fluid>
-        <p>hi</p>
-      </Container>
+      <form onSubmit={handleSubmit} className="editor-container">
+        <Editor
+          initialValue="<p>This is the initial content of the editor.</p>"
+          apiKey="83y8lsfnuk86q6vm9llhy54rxn9c9oa2z8ttovkq59fix13d"
+          onEditorChange={handleChange}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist autolink lists link image charmap print preview anchor',
+              'searchreplace visualblocks code fullscreen',
+              'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar: 'undo redo | formatselect | ' +
+              'bold italic backcolor | alignleft aligncenter ' +
+              'alignright alignjustify | bullist numlist outdent indent | ' +
+              'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+          }}
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </>
   );
 }
