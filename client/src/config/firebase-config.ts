bc72af785a5a6d13810dc461070faf0888724907
chi-wei-lien <lien2@purdue.edu>
2022-03-16 16:53:51 -0400

    put id and api key in .env files

--- client/src/config/firebase-config.ts
+++ client/src/config/firebase-config.ts
@@ -1,14 +1,13 @@
 import firebase from 'firebase';
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCWJehgptnwW9kt-IcLyXJUELVFuL1C3lc",
-  authDomain: "chi-wei-lien.firebaseapp.com",
-  projectId: "chi-wei-lien",
-  storageBucket: "chi-wei-lien.appspot.com",
-  messagingSenderId: "23895404521",
-  appId: "1:23895404521:web:1444e30ed2e45b7e5ba578",
-  measurementId: "G-2RVLJ3WFJW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURE_ID
 };
 
 firebase.initializeApp(firebaseConfig);
