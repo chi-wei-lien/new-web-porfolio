dc7d50af741ed89edf3a6054347e9b173efc7e1f
chi-wei-lien <lien2@purdue.edu>
2022-04-23 17:09:22 -0400

    add code smaple

--- client/src/components/blogs/blogEdit.tsx
+++ client/src/components/blogs/blogEdit.tsx
@@ -131,13 +131,14 @@ const BlogEdit = () => {
               menubar: false,
               plugins: [
                 'advlist autolink lists link image charmap print preview anchor',
-                'searchreplace visualblocks code fullscreen',
+                'searchreplace visualblocks code fullscreen codesample',
                 'insertdatetime media table paste code help wordcount code table blockquote'
               ],
               toolbar: 'undo redo | ' +
                 'bold italic backcolor | alignleft aligncenter ' +
                 'alignright alignjustify | bullist numlist outdent indent | ' +
-                'removeformat | help | styleselect | link image | code| table | blockquote',
+                'removeformat | help | styleselect | link image | code| table | blockquote' +
+                '| codesample',
               content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
             }}
             value={content}
